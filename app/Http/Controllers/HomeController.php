d72b1fc3543c3b2462c93abaccaaa181a56ddb17
Terazawa1016 <62538697+Terazawa1016@users.noreply.github.com>
2020-03-23 17:52:38 +0900

    更新

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,6 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
 
-     //ATOMGitHub
     public function index()
     {
       // Userテーブルの情報を全て取得
